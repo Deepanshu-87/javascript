52b2c03a613737645f00464138178591aa4f12ff
Deepanshu-87 <deepanshukumarofficial87@gmail.com>
2024-10-17 09:50:19 +0000

    variables

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -8,4 +8,5 @@ let accountState; //undefined
 prefer not to use var because of issue in block scope and functional scope
 */
 
+console.log(accountId);
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
