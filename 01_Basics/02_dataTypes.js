da84be65995e251fe275a9f7d6f1e8ad42d71eef
Deepanshu-87 <deepanshukumarofficial87@gmail.com>
2024-10-17 09:47:15 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+//alert(3+3) not work in nodejs 
+
+
+
+
+//number => 2 to power 53
+//bigint
+//string = ""
+//boolean => true/false
+//null => standalone value
+//undefined => is a type
+//symbol => unique
+
+console.log(typeof null);   //object(non-primitive data type)
+console.log(typeof undefined); //undefined
+
