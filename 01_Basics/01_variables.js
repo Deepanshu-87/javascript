cbf8061442b1228eb8b52814e0960af1e1a40436
Deepanshu-87 <deepanshukumarofficial87@gmail.com>
2024-10-17 09:22:52 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=123;
+var accountEmail="deepanshu@google.com"
+let accountPassword="12345"
+accountCity="delhi"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
