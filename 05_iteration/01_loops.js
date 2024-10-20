da486f7efa857b7d89e9836e75df7f0659a51566
Deepanshu-87 <deepanshukumarofficial87@gmail.com>
2024-10-20 08:36:00 +0000

    loops (for,while,do-while), break and continue

---
+++ 05_iteration/01_loops.js
@@ -0,0 +1,45 @@
+
+
+// let i=0;
+// for(i=0;i<=10;i++)
+//      console.log(i);
+
+
+ let j;
+ for(j=1;j<=10;j++)
+ {
+    if(j==5)
+       { console.log(`5 is detected`);
+        break;
+       }
+    console.log(j)    
+ }
+
+
+
+
+
+
+
+
+
+// let index = 0
+// // while (index <= 10) {
+// //     console.log(`Value of index is ${index}`);
+// //     index = index + 2
+// // }
+
+// let myArray = ['flash', "batman", "superman"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     //console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// let score = 11
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
