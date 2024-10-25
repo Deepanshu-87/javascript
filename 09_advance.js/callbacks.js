b431130bd3f1aba63f87d79045865a2f50cf5655
Deepanshu-87 <deepanshukumarofficial87@gmail.com>
2024-10-25 18:36:13 +0000

    callback in js

--- 09_advance.js/callbacks.js
+++ 09_advance.js/callbacks.js
@@ -10,8 +10,11 @@ function getData(dataId,getDataNext){
 
 //getDataNext => ()=>{}
 
+    //callback hell
 getData(1,()=>{
+    console.log('getting data 2')
     getData(2,()=>{
+     console.log('getting data 3')
         getData(3)
     })
 })
